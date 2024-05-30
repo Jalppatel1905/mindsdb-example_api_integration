024d79214e0fa416975c25cc734d3b08eb5dc7f2
Jalppatel1905 <151988427+Jalppatel1905@users.noreply.github.com>
2024-05-30 00:36:32 -0400

    Update example_api_handler.py

--- example_api_handler.py
+++ example_api_handler.py
@@ -1,12 +1,12 @@
 import requests
 
-class ExampleAPIHandler:
+class StudentHandler:
     def __init__(self, **kwargs):
         self.args = kwargs
         self.api_key = self.args.get('api_key')
 
     def get_data(self, endpoint):
         headers = {'Authorization': f'Bearer {self.api_key}'}
-        response = requests.get(f'https://api.example.com/{endpoint}', headers=headers)
+        response = requests.get(f'https://api.student.com/{endpoint}', headers=headers)
         response.raise_for_status()
         return response.json()
