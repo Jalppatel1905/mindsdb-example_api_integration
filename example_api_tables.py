ce0c956f8cfd9f3ce6c69394445e8da59c2dbe42
Jalppatel1905 <151988427+Jalppatel1905@users.noreply.github.com>
2024-05-30 00:22:57 -0400

    Update example_api_tables.py

--- example_api_tables.py
+++ example_api_tables.py
@@ -1,1 +1,13 @@
+from mindsdb.tables import Table
 
+class ExampleAPITables:
+    def __init__(self, handler):
+        self.handler = handler
+
+    def get_tables(self):
+        # Here you can implement the logic to retrieve tables from the API
+        # For this example, we will just return a static list of tables
+        return [
+            Table('table1', data=self.handler.get_data('table1')),
+            Table('table2', data=self.handler.get_data('table2'))
+        ]
